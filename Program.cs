ba345b2d1486802c31e5f45c8a46cc8fbf2693c0
OldBeliver <380506338004@yandex.com>
2022-10-06 14:21:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            string passwordCerification;
+            string message = "Quod licet Jovi, non licet bovi";
+
+            int trial = 3;
+            int currentTrial = 3;
+
+            Console.WriteLine("Введите новый пароль:");
+            password = Console.ReadLine();            
+
+            for (int i = 0; i < trial; i++)
+            {
+                Console.WriteLine("Подтвердите пароль:");
+                passwordCerification = Console.ReadLine();
+
+                if (password == passwordCerification)
+                {
+                    Console.WriteLine(message);
+                    i = trial;
+                }
+                else
+                {
+                    currentTrial--;
+                    Console.WriteLine($"Оставшиеся попытки: {currentTrial}");
+                }
+            }
+
+            if (currentTrial == 0)
+            {
+                Console.WriteLine($"Пароль не подтвержден.");
+            }
+        }
+    }
+}
